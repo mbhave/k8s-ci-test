77ae53f9c88ae604db1101a9d5458aba21faad8a
Madhura Bhave <mbhave@pivotal.io>
2020-05-22 14:10:22 -0700

    Initial commit

---
+++ src/main/java/com/example/k8scisample/K8sCiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8scisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sCiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sCiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/k8scisample/K8sCiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8scisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sCiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
